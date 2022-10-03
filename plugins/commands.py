f223c51e31c8366dd41852c24a4080d828ba14c4
crazyspiderat3 <112283064+crazyspiderat3@users.noreply.github.com>
2022-10-04 01:10:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -43,6 +43,9 @@ async def start(client, message):
                     InlineKeyboardButton('🧿Aʙᴏᴜᴛ🧿', callback_data='about')
                    ]]
         reply_markup = InlineKeyboardMarkup(buttons)
+         m=await message.reply_sticker("CAACAgUAAxkBAAIFNGJSlfOErbkSeLt9SnOniU-58UUBAAKaAAPIlGQULGXh4VzvJWoeBA") 
+        await asyncio.sleep(1)
+        await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
@@ -84,6 +87,9 @@ async def start(client, message):
                     InlineKeyboardButton('🧿Aʙᴏᴜᴛ🧿', callback_data='about')
                    ]]
         reply_markup = InlineKeyboardMarkup(buttons)
+         m=await message.reply_sticker("CAACAgUAAxkBAAIFNGJSlfOErbkSeLt9SnOniU-58UUBAAKaAAPIlGQULGXh4VzvJWoeBA") 
+        await asyncio.sleep(1)
+        await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
