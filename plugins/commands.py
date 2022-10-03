6ff2c62810f384853fcd17f473804147744c9689
crazyspiderat3 <112283064+crazyspiderat3@users.noreply.github.com>
2022-10-04 00:50:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,10 +42,14 @@ async def start(client, message):
                     InlineKeyboardButton('⚙Hᴇʟᴘ⚙', callback_data='help'),
                     InlineKeyboardButton('🧿Aʙᴏᴜᴛ🧿', callback_data='about')
                    ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
+        reply_markup = InlineKeyboardMarkup(buttons)      
+        await message.reply_chat_action("Typing")
+        m=await message.reply_sticker("CAACAgUAAxkBAAIFNGJSlfOErbkSeLt9SnOniU-58UUBAAKaAAPIlGQULGXh4VzvJWoeBA") 
+        await asyncio.sleep(1)
+        await m.delete()        
         await message.reply_photo(
             photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            caption=START_TXT.format(message.from_user.mention,temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
@@ -83,10 +87,14 @@ async def start(client, message):
                     InlineKeyboardButton('⚙Hᴇʟᴘ⚙', callback_data='help'),
                     InlineKeyboardButton('🧿Aʙᴏᴜᴛ🧿', callback_data='about')
                    ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
+        reply_markup = InlineKeyboardMarkup(buttons)      
+        await message.reply_chat_action("Typing")
+        m=await message.reply_sticker("CAACAgUAAxkBAAIFNGJSlfOErbkSeLt9SnOniU-58UUBAAKaAAPIlGQULGXh4VzvJWoeBA") 
+        await asyncio.sleep(1)
+        await m.delete()        
         await message.reply_photo(
             photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            caption=START_TXT.format(message.from_user.mention,temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
