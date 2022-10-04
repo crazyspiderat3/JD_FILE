3652ece99472d4f1ad4de9fcaf50b47290eadc82
crazyspiderat3 <112283064+crazyspiderat3@users.noreply.github.com>
2022-10-04 11:19:18 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -881,7 +881,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap =🎪 ᴛɪᴛɪʟᴇ : {search}
+        cap =f"""🎪 ᴛɪᴛɪʟᴇ : {search}
 
 ┏🎛️𝙶𝙴𝙽𝚁𝙴𝚂  : {genres}
 ┣🎙️𝙻𝙰𝙽𝙶𝚂 : {languages}
@@ -890,7 +890,7 @@ async def auto_filter(client, msg, spoll=False):
 ┣ 🍁𝙶𝚁𝙾𝚄𝙿 : <a href="https://t.me/at3movies"> 𝘈𝘛3𝘮𝘰𝘷𝘪𝘦s</a>
 ┗ 🗑️𝙸𝙽𝙵𝙾 : ᴛʜɪꜱ ᴍᴇꜱꜱᴀɢᴇ ᴡᴀꜱ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴅᴇʟᴇᴛᴇᴅ ɪɴ 5 ᴍɪɴᴜᴛᴇꜱ 
 
-❤️‍🔥𝙼𝙰𝙸𝙽𝚃𝙰𝙸𝙽𝙴𝙳 𝙱𝚈 : <a href="https://t.me/AFxSU">𝐀𝐅𝐱𝐒𝐔</a>
+❤️‍🔥𝙼𝙰𝙸𝙽𝚃𝙰𝙸𝙽𝙴𝙳 𝙱𝚈 : <a href="https://t.me/AFxSU">𝐀𝐅𝐱𝐒𝐔</a>"""
          if imdb and imdb.get('poster'):
         try:
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
